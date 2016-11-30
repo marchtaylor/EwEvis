af611fba8920bf9ca73c7105bdb00997a0517588
marchtaylor <marchtaylor@gmail.com>
2016-11-30 07:34:07 +0100

    update version num and vignette

--- vignettes/EwEvis_vignette.R
+++ vignettes/EwEvis_vignette.R
@@ -1,29 +1,28 @@
 ## ----setup---------------------------------------------------------------
-library(knitr)
-library(rgl)
 library(EwEvis)
+library(rgl)
+
+## ---- echo=FALSE---------------------------------------------------------
+library(knitr)
 knit_hooks$set(webgl = hook_webgl)
 
-## ----testgl, webgl=TRUE, fig.width = 6, fig.height = 6-------------------
-library(rgl)
-library(EwEvis)
+## ------------------------------------------------------------------------
+# Northern Humboldt Current Ecosystem (La Niña, Tam et al. 2008)
+TEgm <- 0.1014 # mean transfer efficiency (trophic levels 2-4)
+Ts <- c(27165, 22349, 1658, 266.3, 17.45, 0.607, 0.00515, 6e-06) # Throughout
+Bs <- c(62.84, 147.1, 70.05, 20.61, 1.658, 0.084, 0.000657, 1e-06, 0) # Biomass
 
-# data
-TEgm <- 0.1014
-Ts <- c(27165, 22349, 1658, 266.3, 17.45, 0.607, 0.00515, 6e-06)
-Bs <- c(62.84, 147.1, 70.05, 20.61, 1.658, 0.084, 0.000657, 1e-06, 0)
-res <- V2Pdim(V = Ts, TEgm=TEgm)
-pal <- colorRampPalette(c("green", "yellow", "red"), bias=3)
-
-options(fig.height=600, fig.width=600)
+## ----testgl, webgl=TRUE, fig.width = 6, fig.height = 5-------------------
 open3d()
-# par3d(windowRect = c(100,100,600,600))
+# color palatte
+pal <- colorRampPalette(c("green", "yellow", "red"), bias=3)
+# Biomass
 tmp1 <- Vbox3d(V=Bs[1:5], TL.len = 10, col=pal(5), add.scale = FALSE)
-tmp2 <- Vbox3d(V=Ts[1:5], TL.len = 10, col=pal(5), add.scale = TRUE, 
-  shift=c(dist(tmp1[[1]]$vb[1,1:2])*20,0,0), scale.len = 1000^(1/3)
+# Throughput
+tmp2 <- Vbox3d(V=Ts[1:5], TL.len = 10, col=pal(5), add.scale = TRUE,  shift=c(dist(tmp1[[1]]$vb[1,1:2])*20,0,0), scale.len = 1000^(1/3)
 )
 
-## ----testgl2, webgl=TRUE, fig.width = 6, fig.height = 6------------------
+## ----testgl2, webgl=TRUE, fig.width = 6, fig.height = 5------------------
 open3d()
 tmp <- Vpyramid3d(V=Ts[2:8], TEgm = 0.07, col=2:7, 
     add.scale = FALSE
